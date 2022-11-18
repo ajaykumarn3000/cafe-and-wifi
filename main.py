8dd3941196a5a41c07d2239ed9f724efda71d90f
Ajaykumar <ajaykumarn3000@gmail.com>
2022-11-18 08:59:02 +0530

    made responsive, made contact.html functional.

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail, Message
+import os
 
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'any-secret-key-you-choose'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = EMAIL_ID = os.environ.get("EMAIL")
+app.config['MAIL_PASSWORD'] = os.environ.get('PASSWORD')
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
 db = SQLAlchemy(app)
 
 
@@ -24,6 +34,12 @@ class Cafes(db.Model):
     can_take_calls = db.Column(db.Boolean, nullable=False)
     seats = db.Column(db.String(10), nullable=False)
     coffee_price = db.Column(db.String(10), nullable=False)
+
+
+def contact_mail(email, message):
+    msg = Message(f"You Got an Message from Cafe & Wifi", sender=EMAIL_ID, recipients=[os.environ.get("ADMIN_MAIL")])
+    msg.html = f'''From: <h3>{email}</h3>\n\n<h4>{message}</h4>'''
+    mail.send(msg)
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -45,14 +61,20 @@ def get_cafe(cafe_id):
     return render_template("cafe.html", cafe=cafe_)
 
 
-@app.route('/about', methods=['GET'])
+@app.route('/about', methods=['GET', 'POST'])
 def about():
     return render_template("about.html")
 
 
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
-    return render_template("contact.html")
+    if request.method == 'POST':
+        print("hello")
+        email = request.form["email"]
+        message = request.form["message"]
+        contact_mail(email, message)
+        return render_template("contact.html", msg="Message Sent!")
+    return render_template("contact.html", msg="Contact Me.")
 
 
 if __name__ == "__main__":
